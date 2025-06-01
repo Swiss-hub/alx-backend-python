df76c49c1fc3a5795be29ba10537b1b15aaac567
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-06-01 17:41:40 +0100

    Parameterize a unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,7 @@
+@parameterized.expand([
+    ("nested_map_simple", {"a": 1}, ("a",), 1),
+    ("nested_map_nested", {"a": {"b": 2}}, ("a",), {"b": 2}),
+    ("nested_map_deep", {"a": {"b": 2}}, ("a", "b"), 2),
+])
+def test_access_nested_map(self, name, nested_map, path, expected):
+    self.assertEqual(access_nested_map(nested_map, path), expected)

f422c0f15eed286ac17b71a443db57dccde4f873
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-06-01 22:30:14 +0100

    Project set up

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        participants = request.data.get('participants', [])
+        if len(participants) < 2:
+            return Response({'detail': 'A conversation must have at least two participants.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participants)
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=['post'], url_path='send-message')
+    def send_message(self, request, pk=None):
+        conversation = self.get_object()
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(
+                sender=request.user,
+                conversation=conversation
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)

889cab9b1c7cd446a9a525d64f9f3321ac0b6074
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-06-01 19:26:04 +0100

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import unittest
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized

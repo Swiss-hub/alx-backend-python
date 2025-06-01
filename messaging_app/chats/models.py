55bc87fcd3b35b944d19d02367bb5ff61c400053
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-06-01 22:22:21 +0100

    Project set up

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']

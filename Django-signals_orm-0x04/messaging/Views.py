d1b4f4f3c4807b40d93037add4ba9527f82e446a
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-06-15 18:12:36 +0100

    Use Signals for Deleting User-Related Data

---
+++ Django-signals_orm-0x04/messaging/Views.py
@@ -0,0 +1,16 @@
+# Django-Chat/Views.py
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.http import HttpResponseForbidden
+
+User = get_user_model()
+
+@login_required
+def delete_user(request):
+    if request.method == 'POST':
+        user = request.user
+        user.delete()
+        return redirect('account_deleted')  # Or any page you want
+    return HttpResponseForbidden("Only POST allowed.")
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,9 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from .models import Message, Notification, MessageHistory
+
+User = get_user_model()
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -23,3 +26,13 @@ def log_message_edit(sender, instance, **kwargs):
                 instance.edited = True
         except Message.DoesNotExist:
             pass
+
+@receiver(post_delete, sender=User)
+def cleanup_user_related_data(sender, instance, **kwargs):
+    # Delete all messages sent or received by the user
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    # Delete all notifications for the user
+    Notification.objects.filter(user=instance).delete()
+    # Delete all message histories edited by the user
+    MessageHistory.objects.filter(edited_by=instance).delete()
